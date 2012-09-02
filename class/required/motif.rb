dc29a4d8c841d90ddde89a1b87bd3684ce1f48ab
PhilippePerret <philippe.perret@yahoo.fr>
2012-09-02 12:19:15 +0200

    Traitement des motifs pour qu'il modifie l'objet ou en envoie un nouveau

--- class/required/motif.rb
+++ class/required/motif.rb
@@ -38,24 +38,90 @@ class Motif
   # => Retourne le motif baissé du nombre de +demitons+
   # @todo: plus tard, pourra modifier par degré, en restant dans la
   # gamme
-  def moins demitons
+  # 
+  # @return une NOUVELLE instance de motif
+  def moins demitons, params = nil
     # La question qui se pose ici est : 
     # Est-ce vraiment le bon moyen de repérer les notes dans un
     # motif ?
-    @motif.gsub(/\b([a-g](is|es)?(is|es)?)/){
+    new_motif = @motif.gsub(/\b([a-g](is|es)?(is|es)?)/){
       note = $1
       # debug "note dans moins : #{note}"
       LINote::new(note).moins(demitons, :tonalite => SCORE::key)
     }
+    change_objet_ou_new_instance new_motif, params, true
   end
   # => Retourne le motif monté du nombre de +demitons+
   # @todo: plus tard, pourra modifier par degré, en restant dans la
   # gamme
-  def plus demitons
-    @motif.gsub(/\b([a-g](is|es)?(is|es)?)/){
+  # 
+  # @return une NOUVELLE instance de motif
+  def plus demitons, params = nil
+    new_motif = @motif.gsub(/\b([a-g](is|es)?(is|es)?)/){
       note = $1
       # debug "note dans plus : #{note}"
       LINote::new(note).plus(demitons, :tonalite => SCORE::key)
     }
+    change_objet_ou_new_instance new_motif, params, true
   end
+  
+  # => Retourne le motif avec les notes liées
+  # 
+  # @return L'OBJET LUI-MÊME (contrairement à d'autres méthodes de
+  # transformation)
+  def legato params = nil
+    motif_leg = pose_legato
+    change_objet_ou_new_instance motif_leg, params, false
+  end
+  
+  # => Retourne le motif avec les notes sur-liées (*)
+  # 
+  # (*) Cette méthode est à utiliser quand le motif contient déjà des
+  # liaisons slur
+  # 
+  # @return L'OBJET LUI-MÊME (contrairement à d'autres méthodes de
+  # transformation)
+  def surlegato params = nil
+    motif_leg = pose_legato true
+    change_objet_ou_new_instance motif_leg, params, false
+  end
+  
+  def pose_legato surlegato=false
+    markin  = surlegato ? '\(' : '('
+    markout = surlegato ? '\)' : ')'
+    dmotif = @motif.split(' ') # => vers des notes mais aussi des marques
+    dmotif[0] = "#{dmotif[0]}#{markin}"
+    ilast = dmotif.count - 1
+    while dmotif[ilast].match(/^[a-g]/).nil?
+      ilast -= 1
+    end
+    dmotif[ilast] = "#{dmotif[ilast]}#{markout}"
+    dmotif.join(' ')
+  end
+  
+  # =>  Méthode appelée à la fin de toutes les méthodes, créant une
+  #     nouvelle instance de Motif ou modifiant l'instance courante
+  #     en fonction de la valeur +new_instance+
+  # @param  new_motif   Le nouveau motif obtenu par la méthode
+  # @param  params      Les paramètres envoyés à la méthode
+  # @param  new_defaut  La valeur de :new par défaut
+  def change_objet_ou_new_instance new_motif, params, new_defaut
+    params = set_new_if_not_defined( params, new_defaut )
+    if params[:new] === true
+      Motif::new new_motif
+    else
+      @motif = new_motif
+      self
+    end
+  end
+  
+  # => Définit la valeur de params[:new] si non défini
+  def set_new_if_not_defined params, val_def
+    params ||= {}
+    return params if params.has_key? :new
+    params[:new] = val_def
+    params
+  end
+  
+  
 end
--- test/rspec/spec/class/motif_spec.rb
+++ test/rspec/spec/class/motif_spec.rb
@@ -5,6 +5,10 @@ require 'spec_helper'
 require 'motif'
 
 describe Motif do
+	def repond_a method
+		@m.should respond_to method
+	end
+	
 	before(:all) do
 	  SCORE = Score::new unless defined? SCORE
 	end
@@ -22,9 +26,9 @@ describe Motif do
 		before(:each) do
 		  @m = Motif::new
 		end
-	  it "doit répondre à :to_s" do
-	    @m.should respond_to :to_s
-	  end
+		
+		# :to_s
+	  it "doit répondre à :to_s" do repond_a :to_s end
 		it ":to_s doit renvoyer nil si le motif n'est pas défini" do
 			iv_set(@m, :motif => nil)
 		  @m.to_s.should be_nil
@@ -43,28 +47,105 @@ describe Motif do
 	  before(:each) do
 	    @m = Motif::new "bb g f e,4 bb8"
 	  end
-		it "doit répondre à :moins" do
-		  @m.should respond_to :moins
+	
+		# :change_objet_ou_new_instance
+		it "doit répondre à :change_objet_ou_new_instance" do
+		  repond_a :change_objet_ou_new_instance
+		end
+		it ":change_objet_ou_new_instance doit modifier l'objet si demandé" do
+			@motif = Motif::new "a b c"
+		  @new = @motif.change_objet_ou_new_instance "c b a", nil, false
+			@new.to_s.should == @motif.to_s
+		end
+		it ":change_objet_ou_new_instance doit créer un nouvel objet si demandé" do
+			@motif = Motif::new "a b c"
+		  @new = @motif.change_objet_ou_new_instance "c b a", nil, true
+			@new.to_s.should_not == @motif.to_s
+		end
+		
+		# :set_new_if_not_defined
+		it "doit répondre à :set_new_if_not_defined" do
+		  repond_a :set_new_if_not_defined
+		end
+		it ":set_new_if_not_defined doit définir la valeur de params[:new]" do
+		  params = {}
+			@m.set_new_if_not_defined params, false
+			params[:new].should === false
+		  params = {}
+			@m.set_new_if_not_defined params, true
+			params[:new].should === true
+		  params = {:new => true}
+			@m.set_new_if_not_defined params, false
+			params[:new].should === true
+		  params = {:new => false}
+			@m.set_new_if_not_defined params, true
+			params[:new].should === false
+		end
+		
+		# :moins
+		it "doit répondre à :moins" do repond_a :moins end
+		it ":moins doit retourner l'objet" do
+		  @m.moins(1).class.should == Motif
 		end
 		it ":moins doit donner le motif avec les demi-tons en moins" do
 			iv_set(SCORE, :key => nil)
-		  @m.moins(1).should == "a fis e ees,4 a8"
-			@m.moins(2).should == "aes f ees d,4 aes8"
+		  @m.moins(1).to_s.should == "a fis e ees,4 a8"
+			@m.moins(2).to_s.should == "aes f ees d,4 aes8"
 			iv_set(SCORE, :key => 'G')
-		  @m.moins(1).should == "a fis e dis,4 a8"
-			@m.moins(2).should == "gis f dis d,4 gis8"
+		  @m.moins(1).to_s.should == "a fis e dis,4 a8"
+			@m.moins(2).to_s.should == "gis f dis d,4 gis8"
 		end
-		it "doit répondre à :plus" do
-		  @m.should respond_to :plus
+		
+		# :plus
+		it "doit répondre à :plus" do repond_a :plus end
+		it ":plus doit retourner l'objet" do
+		  @m.plus(1).class.should == Motif
 		end
 		it ":plus doit donner le motif supérieur" do
 			iv_set(SCORE, :key => nil)
-		  @m.plus(1).should == "b aes fis f,4 b8"
-			@m.plus(2).should == "c a g fis,4 c8"
+		  @m.plus(1).to_s.should == "b aes fis f,4 b8"
+			@m.plus(2).to_s.should == "c a g fis,4 c8"
 			iv_set(SCORE, :key => 'Bb')
-		  @m.plus(1).should == "b aes ges f,4 b8"
-			@m.plus(2).should == "c a g ges,4 c8"
+		  @m.plus(1).to_s.should == "b aes ges f,4 b8"
+			@m.plus(2).to_s.should == "c a g ges,4 c8"
+		end
+		it ":plus avec le paramètre :new => false doit modifier l'objet" do
+		  @motif = Motif::new "c d e"
+			@motif.plus(1)
+			iv_get(@motif, :motif).should == "c d e"
+			@motif.plus(1, :new => false)
+			iv_get(@motif, :motif).should == "des ees f"
 		end
 	
-	end
+		# :legato
+		it "doit répondre à :legato" do repond_a :legato end
+		it ":legato doit renvoyer une instance du motif" do
+		  @m.legato.class.should == Motif
+		end
+		it ":legato doit renvoyer une valeur modifiée" do
+		  @mo = Motif::new "a b cis r4 a-^ |"
+			res = @mo.legato
+			res.to_s.should == "a( b cis r4 a-^) |"
+		end
+		it ":legato avec :new => true doit renvoyer un nouveau motif" do
+		  @mo = Motif::new "a b d"
+			@mo.legato
+			iv_get(@mo, :motif).should == "a( b d)"
+		  @mo = Motif::new "a b d"
+			@mo.legato(:new => true)
+			iv_get(@mo, :motif).should == "a b d"
+		end
+
+		# :surlegato
+		it "doit répondre à :surlegato" do repond_a :surlegato end
+		it ":surlegato doit renvoyer une instance du motif" do
+		  @m.surlegato.class.should == Motif
+		end
+		it ":surlegato doit renvoyer une valeur modifiée" do
+		  @mo = Motif::new "a b cis r4 a-^ |"
+			res = @mo.surlegato
+			res.to_s.should == "a\\( b cis r4 a-^\\) |"
+		end
+		
+	end # / transformation du motif
 end
