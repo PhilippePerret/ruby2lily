5c41504a5ff3cb461a2172b93637fb21573fa165
PhilippePerret <philippe.perret@yahoo.fr>
2012-09-03 12:55:31 +0200

    Minor changes

--- test/rspec/spec/class/orchestre_spec.rb
+++ test/rspec/spec/class/orchestre_spec.rb
@@ -46,20 +46,20 @@ describe Orchestre do
 			expect{res = @o.compose(nil)}.to raise_error SystemExit
 		end
 		it ":compose doit lever une erreur s'il manque le nom (constante)" do
-		  code = 	"name\tinstrument\tclef\tton\n" +
-							"-\tPiano\tU4\tG"
+		  code = 	"name\tinstrument\tclef\tton\n" \
+							<< "-\tPiano\tU4\tG"
 			expect{res = @o.compose(code)}.to raise_error SystemExit
 			# @todo: avec l'erreur : Orchestre::ERRORS[:undefined_name]
 		end
 		it ":compose doit lever une erreur si l'instrument n'est pas défini" do
-			code = 	"name\tinstrument\tclef\tton\n" +
-							"FAUX\t-\t-\t-"
+			code = 	"name\tinstrument\tclef\tton\n" \
+							<< "FAUX\t-\t-\t-"
 			expect{res = @o.compose(code)}.to raise_error SystemExit
 			# @todo: avec l'erreur : Orchestre::ERRORS[:instrument_undefined]
 		end
 		it ":compose doit lever une erreur si un instrument n'existe pas" do
-			code = 	"name\tinstrument\tclef\tton\n" +
-							"FAUX\tbadinstrument\t-\t-"
+			code = 	"name\tinstrument\tclef\tton\n" \
+							<< "FAUX\tbadinstrument\t-\t-"
 			expect{res = @o.compose(code)}.to raise_error SystemExit
 		 	# @todo: avec l'erreur : Orchestre::ERRORS[:unknown_instrument]
 		end
