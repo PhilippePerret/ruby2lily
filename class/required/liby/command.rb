bddbf180d2dd8f48295925998f2fae0991abedda
PhilippePerret <philippe.perret@yahoo.fr>
2012-09-04 11:52:59 +0200

    Implémentation des premières commandes (help et version)

--- class/required/liby/command.rb
+++ class/required/liby/command.rb
@@ -10,19 +10,35 @@ class Liby::Command
   # -------------------------------------------------------------------
   #   Classe
   # -------------------------------------------------------------------
-  
-  # -------------------------------------------------------------------
-  #   Instance
-  # -------------------------------------------------------------------
-  @command = nil    # Le nom string de la commande
-  
-  # Instancie une commande
-  def initialize command
-    @command = command
-  end
-  
-  # Lancement de la commande
-  def run
+  class << self
     
+    def run commande, params = nil
+      eval("run_#{commande}")
+    end
+    
+    # -------------------------------------------------------------------
+    #   Launchers de commande
+    # -------------------------------------------------------------------
+
+    # => Affiche l'aide de l'application
+    def run_help
+      path  = File.join(BASE_LILYPOND, 'HELP.md')
+      texte = File.read(path)
+      puts "\n\n#{texte}"
+      texte # pour les tests
+    end
+    
+    # => Renvoie la version et des informations en console
+    def run_version
+      require File.join(BASE_LILYPOND, 'VERSION.rb')
+      version = "- version: #{RUBY2LILY_VERSION}"
+      auteur  = "- author: Philippe Perret <philippe.perret@yahoo.fr" 
+      github  = "- github: https://github.com/PhilippePerret/ruby2lily"
+      texte = "\nruby2site #{version}\n#{auteur}\n#{github}\n\n"
+      
+      puts texte
+      texte # pour tests
+    end
   end
+  
 end
--- test/rspec/spec/ruby2lily/liby/command_spec.rb
+++ test/rspec/spec/ruby2lily/liby/command_spec.rb
@@ -12,19 +12,29 @@ describe Liby::Command do
 	# 	Classe
 	# -------------------------------------------------------------------
 	describe "Classe" do
-	  
-	end # / classe
-	
-	# -------------------------------------------------------------------
-	# 	Instance
-	# -------------------------------------------------------------------
-	describe "Instance" do
-		before(:each) do
-		  @cmd = Liby::Command::new "commande"
-		end
 	  it "doit répondre à :run" do
-	    @cmd.should respond_to :run
+	    Liby::Command.should respond_to :run
 	  end
-	end # / instance
+	end # / classe
+	describe "Commande" do
+	  it ":run_help doit exister" do
+	    Liby::Command.should respond_to :run_help
+	  end
+		it ":run_help doit faire son travail" do
+		  res = Liby::Command::run_help
+			res.should =~ /Aide ruby2lily/
+		end
+	  it ":run_version doit exister" do
+	    Liby::Command.should respond_to :run_version
+	  end
+		it ":run_version doit faire son travail" do
+		  res = Liby::Command::run_version
+				# @note: `res` n'existe ici que parce que la méthode renvoie
+				# le texte pour les tests. Sinon, il écrit avec puts en console
+			res.should =~ /version: /
+			res.should =~ /author: /
+			res.should =~ /github: /
+		end
+	end
 	
 end
