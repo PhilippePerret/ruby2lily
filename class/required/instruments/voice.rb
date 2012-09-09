9abe74f7545b9e18758576aa32bb3b9afa9b597e
PhilippePerret <philippe.perret@yahoo.fr>
2012-09-09 21:43:23 +0200

    Aide-mémoire pour développement futur (paroles)

--- class/required/instruments/voice.rb
+++ class/required/instruments/voice.rb
@@ -1,10 +1,48 @@
-# 
-# Class Voice < Instrument
-# 
-# Un instrument "Voice" est forcément composé d'une mélodie et
-# de paroles.
-# 
+=begin
 
+  Class Voice < Instrument
+
+  Un instrument "Voice" est forcément composé d'une mélodie et
+  de paroles.
+
+
+  @todo: si la voix a des paroles, on doit l'afficher sous la forme :
+  <<
+    \relative ... {
+    
+    }
+    \addlyrics {
+        ... ici les paroles ...
+    }
+  >>
+  
+  OU (qui d'après le manuel est moins limité) :
+  <<
+    \new Voice = "notes_voix" {
+      \\relative c' { 
+        ... notes ici ...
+      }
+    }
+    \new Lyrics \lyricsto "notes_voix" {
+      ... paroles ici ...
+    }
+  >>
+  
+  @todo: traitement de plusieurs voix :
+  <<
+    \relative .. {
+    
+    }
+    \addlyrics
+  >>
+  En fait, ça revient à traiter comme une voix, mais en mettant tout
+  dans un grand <<..>>
+  => Quand un instrument de type Voice est créé, il faut le mémoriser
+  dans la liste des voix, et faire ce grand bloc lilypond quand on
+  demande l'affichage (donc il ne faut pas passer en revue toutes les
+  voix)
+  
+=end
 class Voice < Instrument
   
   # -------------------------------------------------------------------
