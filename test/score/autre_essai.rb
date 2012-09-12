58131999ba08b36d5de76810715bc1d7397e00fe
PhilippePerret <philippe.perret@yahoo.fr>
2012-09-12 20:40:00 +0200

    Minor changes

--- test/score/autre_essai.rb
+++ test/score/autre_essai.rb
@@ -15,24 +15,35 @@ def orchestre
   EOO
 end
 
+class Salome
+  class << self
+    def riff_ref
+      @riff_ref ||= lambda {
+        suite =     "r4 b( a8 d, a' b~ b4)"   \
+              <<  " a4( a8 d, a'8 g~ g4)"     \
+              <<  " g4( fis8 b, fis' g~ g4)"  \
+              <<  " a4( a8 d, a'8 b~ b4)"
+        Motif::new suite
+      }.call
+    end
+    def riff_ref_main_gauche
+      @riff_ref_main_gauche ||= lambda {
+        riff_ref[:octave => 2, :clef => 'g']
+      }.call
+    end
+
+    def riff_couplet
+      suite = "e4. d4. d" \
+              << " g,1 fis2. fis8 fis a g8"
+      Motif::new suite
+    end
+  end
+end
+
 def score
   # Refrain / Intro
-  SALOME.droite << riff_refrain
-  SALOME.gauche << riff_refrain[1]
+  SALOME.droite << Salome::riff_ref
+  SALOME.gauche << Salome::riff_ref_main_gauche
   # Couplet
-  SALOME.gauche << riff_couplet
-end
-
-def riff_refrain
-  suite =     "r4 b( a8 d, a' b~ b4)"   \
-          <<  " a4( a8 d, a'8 g~ g4)"     \
-          <<  " g4( fis8 b, fis' g~ g4)"  \
-          <<  " a4( a8 d, a'8 b~ b4)"
-  Motif::new suite
-end
-
-def riff_couplet
-  suite = "e4. d4. d" \
-          << " g,1 fis2. fis8 fis a g8"
-  Motif::new suite
+  SALOME.gauche << Salome::riff_couplet
 end
