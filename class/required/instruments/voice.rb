ead9d7ca7c4755dfc29f9d511e619830142c8863
PhilippePerret <philippe.perret@yahoo.fr>
2012-10-01 11:45:57 +0200

    Insertion des paroles dans un instrument de classe Voice

--- class/required/instruments/voice.rb
+++ class/required/instruments/voice.rb
@@ -46,14 +46,95 @@
 class Voice < Instrument
   
   # -------------------------------------------------------------------
+  #   Classe
+  # -------------------------------------------------------------------
+  @@index_voice = nil     # Pour numéroter les voix si pas de nom
+  
+  def self.uniq_name
+    @@index_voice ||= 0
+    @@index_voice += 1
+    "voice_unnamed_#{@@index_voice}"
+  end
+  
+  # -------------------------------------------------------------------
   #   L'instance
   # -------------------------------------------------------------------
-  @paroles        = nil
+  @paroles        = nil   # Instance Voice::Lyrics pour les paroles
   
   def initialize data = nil
-    
     super( data )
     @octave_defaut  = 4
   end
   
+  # => Retourne le code à inscrire dans la partition
+  # 
+  # @note: overwrite la méthode Instrument
+  def to_lilypond params = nil
+    return false unless @displayed
+    @staff = Staff::new(
+                        :clef         => @clef, 
+                        :tempo        => SCORE.tempo, 
+                        :base_tempo   => SCORE.base_tempo,
+                        :octave_clef  => @octave_clef
+                        )
+
+    <<-EOS
+    
+<<
+  \\new Voice = "#{uniq_name}" {
+    #{staff_header(params).gsub(/\n/, "\n\t")}
+    #{staff_content.gsub(/\n/, "\n\t")}
+  }
+  \\new Lyrics \\lyricsto "#{uniq_name}" {
+    #{paroles.to_s}
+  }
+>>
+
+    EOS
+  end
+  
+  # Retourne un nom unique pour la voix
+  def uniq_name
+    @uniq_name ||= lambda {
+      if @name.nil? 
+        Voice::uniq_name 
+      else
+        @name.downcase.gsub(/ /, '_').gsub(/[^a-z_]/,'')
+      end
+    }.call
+  end
+  
+  def paroles
+    @paroles ||= Lyrics::new self
+  end
+  alias :lyrics :paroles
+  
+  
+  # -------------------------------------------------------------------
+  #   La sous-classe Lyrics
+  # -------------------------------------------------------------------
+  class Lyrics
+    
+    # -------------------------------------------------------------------
+    #   Instance
+    # -------------------------------------------------------------------
+    @ivoice = nil     # L'instance Voice des paroles de l'instance
+    @lyrics = nil     # Les paroles, en string
+    # => Instanciation des paroles de la chanson
+    def initialize ivoice = nil
+      @ivoice = ivoice
+    end
+    
+    # => Ajoute des paroles pour la Voice
+    def add paroles
+      @lyrics ||= ""
+      @lyrics = "#{@lyrics} #{paroles}".strip
+    end
+    alias :<< :add
+    
+    # => Retourne les paroles en simple string
+    def to_s
+      @lyrics
+    end
+  end
 end
--- test/rspec/spec/class/instruments/voice_spec.rb
+++ test/rspec/spec/class/instruments/voice_spec.rb
@@ -5,6 +5,10 @@ require 'spec_helper'
 require 'instruments/voice'
 
 describe Voice do
+	before(:all) do
+	  SCORE = Score::new unless defined? SCORE
+		ORCHESTRE = Orchestre::new unless defined? ORCHESTRE
+	end
   # -------------------------------------------------------------------
 	# Classe
 	# -------------------------------------------------------------------
@@ -15,6 +19,13 @@ describe Voice do
 		it ":new doit renvoyer une voice" do
 		  Voice.new.class.should == Voice
 		end
+		it "doit répondre à :uname" do
+		  Voice.should respond_to :uniq_name
+		end
+		it ":uname doit retourner un nom unique" do
+		  Voice.uniq_name.should == "voice_unnamed_1"
+			Voice.uniq_name.should == "voice_unnamed_2"
+		end
 	end # / La classe
 	
 	# -------------------------------------------------------------------
@@ -24,5 +35,88 @@ describe Voice do
 	  before(:each) do
 	    @voice = Voice::new
 	  end
+		describe "Méthodes générales" do
+		  it "doit répondre à :uniq_name" do
+		    @voice.should respond_to :uniq_name
+		  end
+			it ":uniq_name doit renvoyer la bonne valeur" do
+				current = cv_get(Voice, :index_voice)
+			  @voice.uniq_name.should == "voice_unnamed_#{current + 1}"
+				iv_set(@voice, :name => "Ma_Voix", :uniq_name => nil)
+			  @voice.uniq_name.should == "ma_voix"
+			end
+			it ":uniq_name doit supprimer tout ce qui n'est pas a-z_" do
+			  iv_set(@voice, :name => "Une très mauvaise voix, ça…")
+				@voice.uniq_name.should == "une_trs_mauvaise_voix_a"
+			end
+		end
+		describe "Génération du code LilyPond" do
+		  it "doit répondre à :to_lilypond" do
+		    @voice.should respond_to :to_lilypond
+				# @note: le fait de toute façon, par la classe mère Instrument
+		  end
+			it ":to_lilypond doit retourner un code particulier" do
+			  res = @voice.to_lilypond
+				name = @voice.uniq_name
+				res.should =~ /new Voice = "#{name}"/
+				res[0..20].should =~ /<</
+				res[-20..-1].should =~ />>/
+				res.should =~ /\\new Lyrics \\lyricsto "#{name}"/
+			end
+			it ":to_lilypond doit écrire les notes et les accords" do
+			  @voice << "c8 c c d e4 d"
+				@voice.paroles << "Au clair de la lu- ne"
+				res = @voice.to_lilypond
+				res.should =~ /c8 c c d e4 d/
+				res.should =~ /Au clair de la lu- ne/
+			end
+		end
+		
+		describe "Paroles" do
+		  it "doit répondre à :paroles et :lyrics" do
+		    @voice.should respond_to :paroles
+				@voice.should respond_to :lyrics
+		  end
+			it "@paroles doit être une instance Voice::Lyrics" do
+			  paroles = @voice.paroles
+				paroles.class.should == Voice::Lyrics
+			end
+			it "@paroles doit répondre à :add et :<<" do
+			  paroles = @voice.paroles
+				paroles.should respond_to :add
+				paroles.should respond_to :<<
+				# @NOTE: les autres tests sont faits ci-dessous, pour 
+				# Voice::Lyrics
+			end
+			it "On doit pouvoir ajouter des paroles à la Voice" do
+			  @voice.paroles.add "Mes paroles"
+				@voice.paroles << "et autres paroles"
+				@voice.paroles.to_s.should == "Mes paroles et autres paroles"
+			end
+		end
+		
 	end # / L'instance
+	
+	describe "Voice::Lyrics" do
+		before(:each) do
+		  @vl = Voice::Lyrics::new
+		end
+	  it "doit répondre à :add et :<<" do
+	    @vl.should respond_to :add
+			@vl.should respond_to :<<
+	  end
+		it ":add doit ajouter des paroles" do
+		  @vl.add "Mes pa- ro- les"
+			iv_get(@vl, :lyrics).should == "Mes pa- ro- les"
+			@vl.add "et suivantes"
+			iv_get(@vl, :lyrics).should == "Mes pa- ro- les et suivantes"
+		end
+		it "doit répondre à :to_s" do
+		  @vl.should respond_to :to_s
+		end
+		it ":to_s doit retourner les paroles en string" do
+		  @vl.add "Mes paroles"
+			@vl.to_s.should == "Mes paroles"
+		end
+	end
 end
